cf09643ca8b83687ced6c76103df255d85931d90
chenxin <zhangbo0722@126.com>
2021-04-27 17:22:48 +0800

    hot-fix first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello,git version01");
         System.out.println("hello,git version02");
         System.out.println("hello,git version03");
+        System.out.println("hello,git version04-hot-fix");
     }
 
 }
