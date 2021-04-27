547a602647a257f71db1a9bd21a127f5fc7228d1
chenxin <zhangbo0722@126.com>
2021-04-27 16:49:40 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @author: chenxin
+ * @date: 2021-04-27 16:41
+ * @desc:
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello,git version01");
+    }
+
+}
