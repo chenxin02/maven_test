393e6c2c75b62cd10e4c603bde244c9ae46feed8
chenxin <zhangbo0722@126.com>
2021-04-27 16:55:24 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,12 +3,13 @@ package com.atguigu.git;
 /**
  * @author: chenxin
  * @date: 2021-04-27 16:41
- * @desc:
+ * @desc: git测试
  */
 public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("hello,git version01");
+        System.out.println("hello,git version02");
     }
 
 }
