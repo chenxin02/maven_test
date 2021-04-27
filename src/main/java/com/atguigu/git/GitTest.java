1d79d5896ca3f62f54fbe198faea74f1a3bf6280
chenxin <zhangbo0722@126.com>
2021-04-27 20:49:08 +0800

    push test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("hello,git version02");
         System.out.println("hello,git version03");
         System.out.println("hello,git version04-hot-fix");
+        System.out.println("push test");
     }
 
 }
